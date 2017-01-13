a087b689ce8b0be67bfb4124672de6d5ebfec7b7
Orlando L Otero <orly_otero@yahoo.com>
2017-01-13 17:07:13 +0000

    Changed - Partial - Configuring beans in a way that Dao tests don't depend on web

--- src/main/java/com/asimio/demo/main/Application.java
+++ src/main/java/com/asimio/demo/main/Application.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.asimio.demo.config.dvdrental.MultiTenantDvdRentalProperties;
 
 @SpringBootApplication(
 	exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class },
-	scanBasePackages = { "com.asimio.demo.config", "com.asimio.demo.rest" }
+	scanBasePackages = { "com.asimio.demo.config.dvdrental", "com.asimio.demo.rest" }
 )
-@EnableConfigurationProperties(MultiTenantDvdRentalProperties.class)
 public class Application {
 
 	public static void main(String[] args) {
